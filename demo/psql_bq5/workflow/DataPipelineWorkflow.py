adb59be50da78f3b117e1bb75433aad3c32364e7
vishnuchappidi12 <vishnuchappidi12@gmail.com>
2025-10-06 01:29:41 -0700

    Delete demo directory

--- demo/psql_bq5/workflow/DataPipelineWorkflow.py
+++
@@ -1,8 +0,0 @@
-class DataPipelineWorkflow:
-    """
-    This is the mandatory entry point (programreference) for the pipeline.
-    It must exactly match the expected class name for execution.
-    """
-    def __init__(self):
-        # Placeholder for your pipeline initialization code.
-        pass
